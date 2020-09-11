2d08406a78137f2af93fd03c88415e2b7dbdb10f
inpyeong <jinipyung@gmail.com>
2020-09-12 00:46:26 +0900

    스프링 데이터 Common 10. QueryDSL 연동(레포지토리 커스텀 안함)

---
+++ src/test/java/me/tozl/querydsldemo/account/AccountRepositoryTest.java
@@ -0,0 +1,35 @@
+package me.tozl.querydsldemo.account;
+
+import com.querydsl.core.types.Predicate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AccountRepositoryTest {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void crud() {
+        QAccount account = QAccount.account;
+        Predicate predicate = account
+                .firstName.containsIgnoreCase("inpyeong")
+                .and(account.lastName.startsWith("Lee"));
+
+        Optional<Account> one = accountRepository.findOne(predicate);
+        assertThat(one).isEmpty();
+    }
+
+
+}
