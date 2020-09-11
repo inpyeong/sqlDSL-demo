dd80060db8b5536c42c5215d5726d9c82352f3a7
inpyeong <jinipyung@gmail.com>
2020-09-11 20:56:47 +0900

    Initial commit

---
+++ src/main/java/me/tozl/querydsldemo/QuerydsldemoApplication.java
@@ -0,0 +1,13 @@
+package me.tozl.querydsldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydsldemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydsldemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/tozl/querydsldemo/QuerydsldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tozl.querydsldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydsldemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
