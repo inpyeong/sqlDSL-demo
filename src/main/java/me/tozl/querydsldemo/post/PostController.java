60c96abcdc6d008e71eaf291f42d1ade58809d81
inpyeong <jinipyung@gmail.com>
2020-09-13 02:01:10 +0900

    스프링 데이터 Common12. 웹 기능 2부 DomainClassConverter

---
+++ src/main/java/me/tozl/querydsldemo/post/PostController.java
@@ -0,0 +1,25 @@
+package me.tozl.querydsldemo.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class PostController {
+
+    private final PostRepository postRepository;
+
+    public PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @GetMapping("/posts/{id}")
+    public String getPost(@PathVariable("id") Post post) {
+//        Optional<Post> byId = postRepository.findById(id);
+//        Post post = byId.get();
+        return post.getTitle();
+    }
+}
