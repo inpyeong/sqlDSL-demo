05c959c7881b0d5cd842d763fed60fd1a6a6f839
inpyeong <jinipyung@gmail.com>
2020-10-05 21:47:37 +0900

    스프링 데이터 JPA 5. Named Parameter와 SpEL

--- src/main/java/me/tozl/querydsldemo/post/PostRepository.java
+++ src/main/java/me/tozl/querydsldemo/post/PostRepository.java
@@ -3,6 +3,7 @@ package me.tozl.querydsldemo.post;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,7 +11,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByTitleStartsWith(String title);
 
-    @Query("SELECT p from Post AS p WHERE p.title = ?1")
-    List<Post> findByTitle(String title, Sort sort);
+    @Query("SELECT p from Post AS p WHERE p.title = :title")
+    List<Post> findByTitle(@Param("title") String keyword, Sort sort);
 
 }
