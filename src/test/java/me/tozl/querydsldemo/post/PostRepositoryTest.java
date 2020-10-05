da5bb51ac9355010f16d7bc41f6cdb7b24ed79d2
inpyeong <jinipyung@gmail.com>
2020-10-05 09:00:36 +0900

    스프링 데이터 JPA 2. JpaRepository.save() 메소드

--- src/test/java/me/tozl/querydsldemo/post/PostRepositoryTest.java
+++ src/test/java/me/tozl/querydsldemo/post/PostRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,17 +29,34 @@ public class PostRepositoryTest {
         assertThat(all.size()).isEqualTo(1);
     }
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Test
     public void save() {
         Post post = new Post();
-        post.setId(1l);
         post.setTitle("jpa");
-        postRepository.save(post);
+        Post savedPost = postRepository.save(post);// persist
 
-//        Post post = new Post();
-//        post.setId(1l);
-//        post.setTitle("jpa");
-//        postRepository.save(post);
+//        assertThat(entityManager.contains(post)).isTrue();
+//        assertThat(entityManager.contains(savedPost)).isTrue();
+//        assertThat(savedPost == post);
+
+        Post postUpdate = new Post();
+        postUpdate.setId(post.getId());
+        postUpdate.setTitle("hibernate");
+        Post updatedPost = postRepository.save(postUpdate);// merge
+
+//        assertThat(entityManager.contains(updatedPost)).isTrue();
+//        assertThat(entityManager.contains(postUpdate)).isFalse();
+//        assertThat(updatedPost == postUpdate);
+
+        /**
+         * 아래 postUpdate 같은 경우 persist 객체가 아니기 때문에 setTitle을 감지하지 못한다.
+         *
+         * 따라서 항상 save method의 리턴 객체를 이용해서 업데이트하도록 하자.
+         */
+//        postUpdate.setTitle("tozl");
 
         List<Post> all = postRepository.findAll();
         assertThat(all.size()).isEqualTo(1);
