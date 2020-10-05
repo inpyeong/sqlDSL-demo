5b3a7bec7f037afe6af1fe6777e761c904206f55
inpyeong <jinipyung@gmail.com>
2020-10-06 00:48:44 +0900

    스프링 데이터 JPA 6. Update 쿼리

--- src/main/java/me/tozl/querydsldemo/post/PostRepository.java
+++ src/main/java/me/tozl/querydsldemo/post/PostRepository.java
@@ -2,6 +2,7 @@ package me.tozl.querydsldemo.post;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,4 +15,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("SELECT p from Post AS p WHERE p.title = :title")
     List<Post> findByTitle(@Param("title") String keyword, Sort sort);
 
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Post p SET p.title = ?1 WHERE p.id = ?2")
+    int updateTitle(String title, Long id);
 }
--- src/test/java/me/tozl/querydsldemo/post/PostRepositoryTest.java
+++ src/test/java/me/tozl/querydsldemo/post/PostRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -70,10 +71,10 @@ public class PostRepositoryTest {
         assertThat(all.size()).isEqualTo(1);
     }
 
-    private void savePost(String s) {
+    private Post savePost(String s) {
         Post post = new Post();
         post.setTitle(s);
-        postRepository.save(post);
+        return postRepository.save(post);
     }
 
     @Test
@@ -82,4 +83,20 @@ public class PostRepositoryTest {
         List<Post> all = postRepository.findByTitle("Spring", JpaSort.unsafe("LENGTH(title)"));
         assertThat(all.size()).isEqualTo(1);
     }
+
+   @Test
+   public void updateTitle() {
+        Post spring = savePost("Spring");
+
+//         String hibernate = "hibernate";
+//         int update = postRepository.updateTitle("hibernate", spring.getId());
+//         assertThat(update).isEqualTo(1);
+//
+//        Optional<Post> byId = postRepository.findById(spring.getId());
+//        assertThat(byId.get().getTitle()).isEqualTo(hibernate);
+
+       spring.setTitle("hibernate");
+       List<Post> all = postRepository.findAll();
+       assertThat(all.get(0).getTitle()).isEqualTo("hibernate");
+   }
 }
