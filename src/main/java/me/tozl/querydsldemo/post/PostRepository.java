fb917fc1b21aa3316a939f7d5ce3ce10a39488e7
inpyeong <jinipyung@gmail.com>
2020-10-05 21:32:09 +0900

    스프링 데이터 JPA 4. Sort

--- src/main/java/me/tozl/querydsldemo/post/PostRepository.java
+++ src/main/java/me/tozl/querydsldemo/post/PostRepository.java
@@ -1,5 +1,6 @@
 package me.tozl.querydsldemo.post;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,6 +11,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByTitleStartsWith(String title);
 
     @Query("SELECT p from Post AS p WHERE p.title = ?1")
-    List<Post> findByTitle(String title);
+    List<Post> findByTitle(String title, Sort sort);
 
 }
--- src/test/java/me/tozl/querydsldemo/post/PostRepositoryTest.java
+++ src/test/java/me/tozl/querydsldemo/post/PostRepositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.JpaSort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityManager;
@@ -77,7 +79,7 @@ public class PostRepositoryTest {
     @Test
     public void findByTitle() {
         savePost("Spring");
-        List<Post> all = postRepository.findByTitle("Spring");
+        List<Post> all = postRepository.findByTitle("Spring", JpaSort.unsafe("LENGTH(title)"));
         assertThat(all.size()).isEqualTo(1);
     }
 }
