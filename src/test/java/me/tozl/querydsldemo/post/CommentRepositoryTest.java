2304688ff0ceb5b94a79c97421c04e7f46238268
inpyeong <jinipyung@gmail.com>
2020-10-06 20:54:53 +0900

    스프링 데이터 JPA 7. EntityGraph

---
+++ src/test/java/me/tozl/querydsldemo/post/CommentRepositoryTest.java
@@ -0,0 +1,36 @@
+package me.tozl.querydsldemo.post;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CommentRepositoryTest {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Test
+    public void getComment() {
+//        Post post = new Post();
+//        post.setTitle("jpa");
+//        Post savedPost = postRepository.save(post);
+//
+//        Comment comment = new Comment();
+//        comment.setComment("comment");
+//        comment.setPost(savedPost);
+
+//        Optional<Comment> byId = commentRepository.findById(1l);
+//        System.out.println(byId.get().getPost());
+        commentRepository.getById(1l);
+        System.out.println("---------------");
+        commentRepository.findById(1l);
+    }
+}
