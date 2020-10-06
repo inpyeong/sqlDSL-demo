f667c727988f392c9b79ac314ad7a69b6ba291b2
inpyeong <jinipyung@gmail.com>
2020-10-07 02:30:37 +0900

    스프링 데이터 JPA 10. Query By Example

--- src/test/java/me/tozl/querydsldemo/post/CommentRepositoryTest.java
+++ src/test/java/me/tozl/querydsldemo/post/CommentRepositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -63,6 +65,18 @@ public class CommentRepositoryTest {
     public void specs() {
 
         Page<Comment> page = commentRepository.findAll(isBest().or(isGood()), PageRequest.of(0, 10));
+
+    }
+
+    @Test
+    public void qbe() {
+        Comment prove = new Comment();
+        prove.setBest(true);
+
+        ExampleMatcher exampleMatcher = ExampleMatcher.matchingAny().withIgnorePaths("up", "down");
+        Example<Comment> example = Example.of(prove, exampleMatcher);
+
+        commentRepository.findAll(example);
 
     }
 }
